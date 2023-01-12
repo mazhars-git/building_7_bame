4341868c2be2251a82c17b90ece96e43d1be2401
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-12 11:09:12 +0600

    com choice

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ const resultDisplay = document.getElementById("result");
 
 const possibleChoices = document.querySelectorAll("button");
 
-let userChoice
+let userChoice;
+let computerChoice;
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComChoice();
 }))
+
+function generateComChoice(){
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    
+    if (randomNumber === 1) {
+        computerChoice = 'Rock';
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'Paper';
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'Scissors';
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
 
